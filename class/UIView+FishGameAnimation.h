b1a76c8ef078a43c7f271284131b6977cc3f44d6
GeekSprite <a1019448557@gmail.com>
2017-10-09 18:52:40 +0800

    remove sth

--- class/UIView+FishGameAnimation.h
+++ class/UIView+FishGameAnimation.h
@@ -3,7 +3,7 @@
 //  DCGame
 //
 //  Created by GeekSprite on 2017/3/28.
-//  Copyright © 2017年 Sawadee. All rights reserved.
+//  Copyright © 2017年  All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
